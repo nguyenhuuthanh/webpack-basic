6d938145bebd444d852098be0a256cfe567b539c
ThanhNguyen <thanh.nguyen@saritasa.com>
2019-03-06 15:40:16 +0700

    Output management: preparation

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
 import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
     let element = document.createElement('div');
+    var btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
     // Add the image to our existing div.
     var myIcon = new Image();
@@ -15,6 +20,8 @@ function component() {
     element.appendChild(myIcon);
 
     console.log(Data);
+
+    element.appendChild(btn);
 
     return element;
 }
